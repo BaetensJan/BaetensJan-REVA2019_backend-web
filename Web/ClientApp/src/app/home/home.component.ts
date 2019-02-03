bae5465b951daf28924d843d4fdaa710228b7c7d
Gilles Willemot <gilles.willemot@gmail.com>
2019-02-03 10:59:12 +0100

    layout changes

--- Web/ClientApp/src/app/home/home.component.ts
+++ Web/ClientApp/src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 }
