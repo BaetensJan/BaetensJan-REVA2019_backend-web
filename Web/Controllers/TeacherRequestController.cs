e46927bfb14eb5458d140cace58caa5eebc4f008
janbaetens <jan.baetens.w6130@student.hogent.be>
2019-03-30 11:14:20 +0100

    Added email to admin when new request has been send

--- Web/Controllers/TeacherRequestController.cs
+++ Web/Controllers/TeacherRequestController.cs
@@ -1,12 +1,9 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using ApplicationCore.Entities;
 using ApplicationCore.Interfaces;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Configuration;
 using Web.DTOs;
 
 namespace Web.Controllers
@@ -19,11 +16,19 @@ namespace Web.Controllers
     public class TeacherRequestController : Controller
     {
         private readonly ITeacherRequestRepository _teacherRequestRepository;
+        private readonly IEmailSender _emailSender;
+        private readonly IConfiguration _configuration;
 
 
-        public TeacherRequestController(ITeacherRequestRepository teacherRequestRepository)
+        public TeacherRequestController(
+            ITeacherRequestRepository teacherRequestRepository,
+            IEmailSender emailSender,
+            IConfiguration configuration
+        )
         {
             _teacherRequestRepository = teacherRequestRepository;
+            _emailSender = emailSender;
+            _configuration = configuration;
         }
 
         /**
@@ -54,6 +59,23 @@ namespace Web.Controllers
             var request = new TeacherRequest(model.Name, model.Surname, model.Email, model.SchoolName, model.Note);
             await _teacherRequestRepository.Add(request);
             await _teacherRequestRepository.SaveChanges();
+
+            await _emailSender.SendMailAsync(_configuration["Email:Smtp:From"],
+                $"Nieuwe aanvraag voor Reva App",
+                $@"
+                            <p>Beste,<p>
+                            <p>Er werd zojuist een aanvraag ingediend voor een nieuwe gebruiker!</p>
+                            <br/>
+                            <br/>
+                            <p>School: {model.SchoolName}</p>
+                            <p>Voornaam: {model.Name}</p>
+                            <p>Naam: {model.Surname}</p>
+                            <p>Note: {model.Note}</p>
+                            <br/>
+                            <br/>
+                            <p>Met vriendelijke groet,</p>
+                            <p>Reva app team</p>
+                    ", new[] {"freddy@reva.be"});
 
             return Ok(new
             {
